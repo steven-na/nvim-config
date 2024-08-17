1bf669880794a478e776bc905dd91cfc09dbf42e
Blake C <github.bsc@proton.me>
2024-08-17 15:19:01 -0700

    Treesitter navigation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,26 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require 'nvim-treesitter.install'.compilers = { "zig" }
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            auto_install = true;
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	config = function()
+		require("nvim-treesitter.install").compilers = { "zig" }
+		local treesitter = require("nvim-treesitter.configs")
+		treesitter.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
