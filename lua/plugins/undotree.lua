43cb6e930a5856694e5fec599390f5a74140b9cf
Blake C <github.bsc@proton.me>
2024-08-18 13:15:07 -0700

    Description

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -3,7 +3,7 @@ return {
 		"mbbill/undotree",
 		config = function()
 			vim.g.undotree_DiffCommand = "FC"
-			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Show undo tree" })
 			vim.o.undofile = true
 		end,
 	},
