9fdea4b4a93c4777c8782512927f0ffe01187ff4
Blake C <github.bsc@proton.me>
2024-11-16 19:40:18 -0800

    bufferline switching

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,6 +8,9 @@ return {
 				diagnostics = "nvim_lsp",
 				separator_style = "slant",
 				themable = true,
+				numbers = function(opts)
+					return string.format("%s·%s", opts.raise(opts.id), opts.lower(opts.ordinal))
+				end,
 				offsets = {
 					{
 						filetype = "neo-tree",
@@ -21,5 +24,14 @@ return {
 		})
 
 		vim.keymap.set("n", "<Leader>s", ":BufferLinePick<CR>", { desc = "Switch tabs" })
+		vim.keymap.set("n", "<leader>s1", ":BufferLineGoToBuffer 1<CR>")
+		vim.keymap.set("n", "<leader>s2", ":BufferLineGoToBuffer 2<CR>")
+		vim.keymap.set("n", "<leader>s3", ":BufferLineGoToBuffer 3<CR>")
+		vim.keymap.set("n", "<leader>s4", ":BufferLineGoToBuffer 4<CR>")
+		vim.keymap.set("n", "<leader>s5", ":BufferLineGoToBuffer 5<CR>")
+		vim.keymap.set("n", "<leader>s6", ":BufferLineGoToBuffer 6<CR>")
+		vim.keymap.set("n", "<leader>s7", ":BufferLineGoToBuffer 7<CR>")
+		vim.keymap.set("n", "<leader>s8", ":BufferLineGoToBuffer 8<CR>")
+		vim.keymap.set("n", "<leader>s9", ":BufferLineGoToBuffer 9<CR>")
 	end,
 }
